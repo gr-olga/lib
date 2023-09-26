48536c6dec56e65489b8b23df013d351c9867e12
Olga Panfilova <gr-olga@ya.ru>
2023-09-26 15:00:40 +0200

    add two borders design

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './App.css';
 import {Border} from "./components/Border/Border";
 
 function App() {
-  return (
-    <div className="App">
-    <Border/>
-    </div>
-  );
+    return (
+        <div className="App">
+
+                <Border/>
+
+        </div>
+    );
 }
 
 export default App;
--- src/components/Border/Border.tsx
+++ src/components/Border/Border.tsx
@@ -1,10 +1,12 @@
-import {ReactNode} from "react";
 import styles from './border.module.scss'
 
-export const Border =(children:any)=>{
-    return(
-      <div className={styles.mainBorderContainer}>
-hello
-      </div>
+export const Border = (children: any) => {
+    return (
+        <div>
+            <div className={styles.firstBorderContainer}>
+                hello
+            </div>
+            <div className={styles.secondBorderContainer}> there</div>
+        </div>
     )
 }
